399fbf6f11e06e629bfc6cdc66f3c3cb50152a54
meer-shah <shahmeergull20@gmail.com>
2023-02-16 02:41:10 -0800

    commit

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,57 @@
 import React from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Gallery.css';
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const Gallery = () => {
+  // abi scrollref null hy
+  const scrollRef = React.useRef(null);
+  // This line declares a scrollRef variable using the useRef() hook provided by React.
+  //  This hook creates a reference to an element in the DOM that will be used to implement scrolling functionality
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
+
+  // This code defines a function called scroll that takes a direction parameter. It gets the current value of the scrollRef variable (which references the element in the DOM that will be scrolled), and then checks the direction parameter. If the direction is "left", it scrolls the element 300 pixels to the left by subtracting 300 from current.scrollLeft.
+  //  If the direction is "right", it scrolls the element 300 pixels to the right by adding 300 to current.scrollLeft
+
+  return (
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <SubHeading title="Instagram" />
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+        <button type="button" className="custom__button">View More</button>
+      </div>
+      <div className="app__gallery-images">
+        {/* yhan scroll left implimet kia hy */}
+        <div className="app__gallery-images_container" ref={scrollRef}>
+          {[images.gallery01, images.gallery02, images.gallery03, images.gallery04].map((image, index) => (
+            <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
+              <img src={image} alt="gallery_image" />
+              <BsInstagram className="gallery__image-icon" />
+            </div>
+
+// This code creates a container for the images in the gallery. It uses the scrollRef reference to specify that this is the element that should be scrolled when the buttons are clicked. The images are defined as an array of images,
+//  which are mapped over to create image cards containing the image and an Instagram icon
+          ))}
+        </div>
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')} />
+          <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Gallery;
--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images, data } from '../../constants';
 import './Laurels.css';
 
+// award ka function ham ny yhan is lye bnaya hy q k yeh khyen or use ni hoga
+
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
+  <div className="app__laurels_awards-card">
+    <img src={imgUrl} alt="awards" />
+    <div className="app__laurels_awards-card_content">
+      <p className="p__cormorant" style={{ color: '#DCCA87' }}>{title}</p>
+      <p className="p__opensans">{subtitle}</p>
+    </div>
+  </div>
+);
+
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className="app__bg app__wrapper section__padding" id="awards">
+    <div className="app__wrapper_info">
+      <SubHeading title="Awards & recognition" />
+      <h1 className="headtext__cormorant">Our Laurels</h1>
+{/* ye awards ham ny import kiye hain hr award ma ik heading hy ik image hy ik subheading hy  */}
+      <div className="app__laurels_awards">
+        {data.awards.map((award) => <AwardCard award={award} key={award.title} />)}
+      </div>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.laurels} alt="laurels_img" />
+    </div>
   </div>
 );
 
