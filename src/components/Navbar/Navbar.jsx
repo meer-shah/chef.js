d80d25fa662511a5e72571590f77f237f4f64623
meer-shah <shahmeergull20@gmail.com>
2023-02-16 08:03:54 -0800

    commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -40,11 +40,11 @@ return(
           <MdOutlineRestaurantMenu color='#fff'  fontSize ={27} className="overlay__close" onClick={()=>setToggleMenu(false)}/>
           {/* jb close py click kryen gy to false ho jay ga */}
           <ul className="app__navbar-smallscreen_links">
-          <li className="p__opensans"><a href="#home">Home</a></li>
-        <li className="p__opensans"><a href="#about">About</a></li>
-        <li className="p__opensans"><a href="#menu">Menu</a></li>
-        <li className="p__opensans"><a href="#awards">Awards</a></li>
-        <li className="p__opensans"><a href="#contact">Contact</a></li>
+          <li><a href="#home" onClick={() => setToggleMenu(false)}>Home</a></li>
+              <li><a href="#about" onClick={() => setToggleMenu(false)}>About</a></li>
+              <li><a href="#menu" onClick={() => setToggleMenu(false)}>Menu</a></li>
+              <li><a href="#awards" onClick={() => setToggleMenu(false)}>Awards</a></li>
+              <li><a href="#contact" onClick={() => setToggleMenu(false)}>Contact</a></li>
          
         </ul>
           </div>)}
