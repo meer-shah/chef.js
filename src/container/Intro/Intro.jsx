5267a74f0df9dbb22d729afa6ccf2fedb2aa8c94
meer-shah <shahmeergull20@gmail.com>
2023-02-16 01:10:11 -0800

    commit

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 
+import { meal } from '../../constants';
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playVideo, setPlayVideo] = React.useState(false);
+  const vidRef = React.useRef();
 
-export default Intro;
+  return (
+    <div className="app__video">
+      <video
+        ref={vidRef}
+        src={meal}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={() => {
+            // if play vedio is false 
+            setPlayVideo(!playVideo);
+// if its true and its clicked to rok do
+            if (playVideo) {
+              vidRef.current.pause();
+            } else
+            // click kro to chl jay
+            {
+              vidRef.current.play();
+            }
+          }}
+        >
+          {/* if play vedio is true to pause ka icon show hoga wrna play ka */}
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro; 
